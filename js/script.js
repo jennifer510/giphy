c077dc5f72368a9a29d80f46a564c93a72d05733
jennifer123 <jenifferamezquita@ccpaedu.com>
2019-03-14 00:50:42 +0000

    update changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-/* global $ */
-
-$("#search-button").click(function(){
+ $("#search-button").click(function(){
+     
+  var  searchTerm = $('#search-term').val();
+    var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+$.ajax({
+   url:request_url,
+   method: "get",
+   success: function(response){
+       //var url= response.data[0].images.original.url;
+       //var url= response.data[2].images.original_still.url;
+       
+       var pic_url = response.data[0].images.original.url;
+           $('body').append('<img src=' + pic_url + '>');
+     //console.log(response);
+       }
+ })
   
-  
-  
-});
-
+ });
